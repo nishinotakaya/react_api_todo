e330e624414fa9149e84a4f34d59ab5857319fd4
nishinotakaya <takaya314boxing@gmail.com>
2023-03-01 13:37:23 +0900

    fix/インデント修正

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,52 +3,52 @@ import { useState } from 'react';
 
 const TodoList = () => {
 
-    const initialState = [
-        {
-            task: 'Learn vue.js',
-            isCompleted: false
-        },
-        {
-            task: 'Learn React Hook',
-            isCompleted: false
-        },
-        {
-            task: 'Learn Gatsby.js',
-            isCompleted: false
-        },     
-    ]
+const initialState = [
+	{
+		task: 'Learn vue.js',
+		isCompleted: false
+	},
+	{
+		task: 'Learn React Hook',
+		isCompleted: false
+	},
+	{
+		task: 'Learn Gatsby.js',
+		isCompleted: false
+	},     
+]
 
-    const [todos, setTodos] = useState(initialState);
-    const [task, setTask] = useState('')
-    const handleNewTask = (event) => {
-        setTask( event.target.value )
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        if(task === '') return
-        setTodos(todos => [...todos,{ task, isCompleted: false}])
-        setTask('')
-    }
+	const [todos, setTodos] = useState(initialState);
+	const [task, setTask] = useState('')
+	const handleNewTask = (event) => {
+		setTask( event.target.value )
+	}
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		if(task === '') return
+		setTodos(todos => [...todos,{ task, isCompleted: false}])
+		setTask('')
+	}
 
-    return (
-        <div>
-            <h1>ToDo List</h1>
-            <form onSubmit={handleSubmit}>
-                Add Task :
-                <input
-                    value={task}
-                    placeholder="Add New Task"
-                    onChange={handleNewTask}
-                />
-                <button type="submit">Add</button>
-            </form>
-            <ul>
-                { todos.map((todo, index) => (
-                <li key={ index }>{ todo.task }</li>
-                ))}
-            </ul>
-        </div>
-    );
+	return (
+		<div>
+			<h1>ToDo List</h1>
+			<form onSubmit={handleSubmit}>
+				Add Task :
+				<input
+					value={task}
+					placeholder="Add New Task"
+					onChange={handleNewTask}
+				/>
+				<button type="submit">Add</button>
+			</form>
+			<ul>
+				{ todos.map((todo, index) => (
+				<li key={ index }>{ todo.task }</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default TodoList;
