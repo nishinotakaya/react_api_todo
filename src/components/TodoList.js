4b8d949c4f15b948c3ca1e402968fc582affc7a6
nishinotakaya <takaya314boxing@gmail.com>
2023-03-01 14:11:48 +0900

    各Todoをコンポーネント化

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,54 +1,47 @@
+import React from 'react';
 
-import { useState } from 'react';
+const TodoList = ({ todos, setTodos }) => {
+  const handleRemoveTask = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
-const TodoList = () => {
+  const handleUpdateTask = (index) => {
+    const newTodos = todos.map((todo, todoIndex) => {
+      if (todoIndex === index) {
+        todo.isCompleted = !todo.isCompleted;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
 
-const initialState = [
-	{
-		task: 'Learn vue.js',
-		isCompleted: false
-	},
-	{
-		task: 'Learn React Hook',
-		isCompleted: false
-	},
-	{
-		task: 'Learn Gatsby.js',
-		isCompleted: false
-	},     
-]
-
-	const [todos, setTodos] = useState(initialState);
-	const [task, setTask] = useState('')
-	const handleNewTask = (event) => {
-		setTask( event.target.value )
-	}
-	const handleSubmit = (event) => {
-		event.preventDefault()
-		if(task === '') return
-		setTodos(todos => [...todos,{ task, isCompleted: false}])
-		setTask('')
-	}
-
-	return (
-		<div>
-			<h1>ToDo List</h1>
-			<form onSubmit={handleSubmit}>
-				Add Task :
-				<input
-					value={task}
-					placeholder="Add New Task"
-					onChange={handleNewTask}
-				/>
-				<button type="submit">Add</button>
-			</form>
-			<ul>
-				{ todos.map((todo, index) => (
-				<li key={ index }>{ todo.task }</li>
-				))}
-			</ul>
-		</div>
-	);
-}
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <li
+          key={index}
+          style={{
+            textDecoration: todo.isCompleted ? 'line-through' : 'none',
+          }}
+        >
+          <input
+            type="checkbox"
+            checked={todo.isCompleted}
+            onChange={() => handleUpdateTask(index)}
+          />
+          {todo.task}
+          <span
+            onClick={() => handleRemoveTask(index)}
+            style={{ cursor: 'pointer' }}
+          >
+            X
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default TodoList;
