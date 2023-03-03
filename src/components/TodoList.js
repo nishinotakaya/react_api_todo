7d45e646825d690fff426ef6bf3cd9726f116308
nishinotakaya <takaya314boxing@gmail.com>
2023-03-03 21:40:17 +0900

    fix/スペース合わせる

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { useEffect } from 'react';
 
 const TodoList = ({ todos, setTodos }) => {
-useEffect(() => {
-fetch('http://localhost:4567/todos')
-.then((response) => response.json())
-.then((data) => {
-setTodos(data);
-})
-.catch((error) => {
-console.error('Error:', error);
-});
-}, );
-function handleRemoveTask(index) {
-const newTodos = [...todos];
-newTodos.splice(index, 1);
-setTodos(newTodos);
-}
+  useEffect(() => {
+    fetch('http://localhost:4567/todos')
+    .then((response) => response.json())
+    .then((data) => {
+    setTodos(data);
+  })
+    .catch((error) => {
+    console.error('Error:', error);
+  });
+  }, );
+	function handleRemoveTask(index) {
+		const newTodos = [...todos];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	}
 
   const handleUpdateTask = (index) => {
     const newTodos = todos.map((todo, todoIndex) => {
